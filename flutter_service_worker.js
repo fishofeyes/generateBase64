fa40eb5ac93634d42a43e5731163587c2a893fdc
zou <774249737@qq.com>
2025-02-10 12:12:12 +0800

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2cc80bba384a864172ec06299ed16f69",
+const RESOURCES = {"flutter_bootstrap.js": "36697eb4da8c39b6e9cf37ed7c0eaa66",
 "version.json": "2ee705bdd718d7141a7d373ca0bd2e82",
 "index.html": "2ede7f907119ac10d241d01b64017eee",
 "/": "2ede7f907119ac10d241d01b64017eee",
-"main.dart.js": "e44c9f583cdfa0f47a7108f369513ff5",
+"main.dart.js": "f03e21dd52ee2d1105914e1fcec8e983",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
